5afda3087b1ff185f8c93b8bde1917cc68dfe535
sap <pechkursasha@gmail.com>
2025-07-30 11:52:13 +0000

    feat: implement methods for user subscription startCommand, subscribe, unsubscribe, setupDailyWeatherTask

--- src/bot/bot.service.ts
+++ src/bot/bot.service.ts
@@ -1,4 +1,55 @@
-import { Injectable } from '@nestjs/common';
+import { InjectBot, Start, Update, Command } from 'nestjs-telegraf';
+import { Context } from 'telegraf';
+import { WeatherService } from '../weather/weather.service';
+import { UsersService } from '../users/users.service';
+import * as cron from 'node-cron';
 
-@Injectable()
-export class BotService {}
+@Update()
+export class BotService {
+  constructor(
+    private weatherService: WeatherService,
+    private usersService: UsersService,
+    @InjectBot() private bot: any
+  ) {
+    // run once after initialization dependencies
+    this.setupDailyWeatherTask();
+  }
+
+  @Start()
+  async startCommand(ctx: Context) {
+    await ctx.reply('Hello type /subscribe to get notification about weather in particular time!');
+  }
+
+  @Command('subscribe')
+  async subscribe(ctx: Context) {
+    const id = ctx.chat?.id;
+    const added = this.usersService.addUser(id);
+    if (added) {
+      await ctx.reply('You have subscribed successfuly!');
+    } else {
+      await ctx.reply('You are  already subscribed! ');
+    }
+  }
+
+  @Command('unsubscribe')
+  async unsubscribe(ctx: Context) {
+    const id = ctx.chat?.id;
+    // remove user from db
+    const removed = this.usersService.removeUser(id);
+    if (removed) {
+      await ctx.reply('You have unsubscribed successfuly!');
+    } else {
+      await ctx.reply('You haven\'\nt subscribed yet! ');
+    }
+  }
+
+  setupDailyWeatherTask() {
+    cron.schedule('0 8 * * *', async () => {
+      const users = this.usersService.getUsers();
+      const weather = await this.weatherService.getWeather();
+      for (const id of users) {
+        await this.bot.telegram.sendMessage(id, weather);
+      }
+    });
+  }
+}
