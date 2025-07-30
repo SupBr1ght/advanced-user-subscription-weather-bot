fa19c50c5893caca58ea0604b593a6cd8015095b
sap <pechkursasha@gmail.com>
2025-07-30 12:38:38 +0000

    fix: add imports HttpModule, ConfigModule to bot.module.ts

--- src/bot/bot.module.ts
+++ src/bot/bot.module.ts
@@ -2,8 +2,11 @@ import { Module } from '@nestjs/common';
 import { BotService } from './bot.service';
 import { WeatherService } from '../weather/weather.service';
 import { UsersService } from '../user/user.service';
+import { HttpModule } from '@nestjs/axios';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
+  imports: [HttpModule, ConfigModule],
   providers: [BotService, WeatherService, UsersService],
 })
-export class BotModule {}
+export class BotModule { }
