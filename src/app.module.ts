97dd214e7e333ab40e6e50394b44f90569b1a8b5
sap <pechkursasha@gmail.com>
2025-07-30 10:30:02 +0000

    feat: add service weather

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { BotModule } from './bot/bot.module';
+import { WeatherService } from './weather/weather.service';
 
 @Module({
   imports: [BotModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, WeatherService],
 })
 export class AppModule {}
