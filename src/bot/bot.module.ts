d3442749c61ef2a2b15da637b951a7b8e436e9dd
sap <pechkursasha@gmail.com>
2025-07-30 11:23:08 +0000

    feat: inject neccessary services into bot module

--- src/bot/bot.module.ts
+++ src/bot/bot.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
 import { BotService } from './bot.service';
+import { WeatherService } from '../weather/weather.service';
+import { UsersService } from '../user/user.service';
 
 @Module({
-  providers: [BotService]
+  providers: [BotService, WeatherService, UsersService],
 })
 export class BotModule {}
