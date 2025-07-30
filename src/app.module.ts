cfdf9d8373579d903de411b82cb25d498324108a
sap <pechkursasha@gmail.com>
2025-07-30 10:30:48 +0000

    feat: add service user

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,11 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { BotModule } from './bot/bot.module';
 import { WeatherService } from './weather/weather.service';
+import { UserService } from './user/user.service';
 
 @Module({
   imports: [BotModule],
   controllers: [AppController],
-  providers: [AppService, WeatherService],
+  providers: [AppService, WeatherService, UserService],
 })
 export class AppModule {}
