4b7539334c83a8e69c798fb97604d38c45a2afd5
sap <pechkursasha@gmail.com>
2025-07-30 13:42:12 +0000

    feat: import schema to user module

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,4 +1,18 @@
-import { Module } from '@nestjs/common';
+// src/user/user.module.ts
 
-@Module({})
-export class UserModule {}
+import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { UserSubscription, UserSubscriptionSchema } from '../models/user.schema';
+import { UsersService } from './user.service';
+
+@Module({
+  imports: [
+    // import user's schema to module
+    MongooseModule.forFeature([
+      { name: UserSubscription.name, schema: UserSubscriptionSchema },
+    ]),
+  ],
+  providers: [UsersService],
+  exports: [UsersService],
+})
+export class UsersModule {}
